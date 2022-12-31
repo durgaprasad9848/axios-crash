769ca5ee2a149a58af2473f10e85a98296676f19
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2022-12-31 12:40:56 +0530

    form-submit

--- helloworld/src/Components/Expenses/NewExpenses/ExpenseForm.js
+++ helloworld/src/Components/Expenses/NewExpenses/ExpenseForm.js
@@ -3,47 +3,60 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [ enteredAmount, setEnteredAmount] = useState('');
-    const [ enteredDate, setEnteredDate] = useState('');
-
-   
+    const[enteredTitle,setEnterdTitle] = useState('');
+    const[enteredAmount,setEnterdAmount] = useState('');
+    const[enteredDate, setEnteredDate] = useState('');
 
     const titleChangeHandler = (event) =>
     {
-        setEnteredTitle(event.target.value);
-        console.log(enteredTitle);
+        setEnterdTitle(event.target.value);
     };
 
-    const amountChangeHandler = (event) =>{
-        setEnteredAmount(event.target.value);
+    const amountChangeHandler = (event) =>
+    {
+        setEnterdAmount(event.target.value);
     };
 
-    const dateChangeHandler = (event) =>{
+    const dateChangeHandler = (event) =>
+    {
         setEnteredDate(event.target.value);
     };
 
 
 
 
+  const submitclick = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+        title : enteredTitle,
+        amount : enteredAmount,
+        date : new Date(enteredDate)
+    };
+    console.log(expenseData);
+  };
+
+
+
+
   return (
-    <form>
+    <form onSubmit={submitclick}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" id ='iptitle' onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__controls">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01"  onChange={amountChangeHandler} />
+          <input type="number" min="0.01" step="0.01" id = "ipamount" onChange={amountChangeHandler}  />
         </div>
         <div className="new-expense__controls">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-01-01"  onChange={dateChangeHandler}/>
+          <input type="date" min="2019-01-01" max="2023-01-01" id = "ipdate" onChange={dateChangeHandler}  />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type = "submit">Add Expense</button>
+        <button type = "submit" >Add Expense</button>
       </div>
     </form>
   );
