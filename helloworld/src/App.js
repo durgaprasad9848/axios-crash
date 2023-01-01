7ff6e85d5b52a278b29ecf8e1c46934ae2c885b5
durgaprasad9848 <pulipatiprasad2000@gmail.com>
2023-01-01 17:22:45 +0530

    Filter

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,96 +1,55 @@
-import "./App.css";
-import Card from "./Components/UI/Card";
-import NewExpense from "./Components/Expenses/NewExpenses/NewExpense.js";
-import ExpenceItem from "./Components/Expenses/ExpenseItem.js";
-import ExpensesFilter from "./Components/Expenses/NewExpenses/ExpensesFilter";
-import React, { useState } from "react";
+import React,{useState} from 'react';
 
-const DummyExpenses = [
+import NewExpense from "./Components/Expenses/NewExpenses/NewExpense.js";
+import Expenses from './Components/Expenses/Expenses.js';
+
+const DummyExpense = [
   {
-    id: "e1",
-    title: "Toilet Paper",
+    id: 'e1',
+    title: 'Toilet Paper',
     amount: 94.12,
     date: new Date(2020, 7, 14),
-    location: "Tenali",
   },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: "e2",
-    title: "New TV",
-    amount: 799.49,
-    date: new Date(2021, 2, 12),
-    location: "Hyderabad",
-  },
-  {
-    id: "e3",
-    title: "Car Insurance",
+    id: 'e3',
+    title: 'Car Insurance',
     amount: 294.67,
     date: new Date(2021, 2, 28),
-    location: "Vijawada",
   },
   {
-    id: "e4",
-    title: "New Desk (Wooden)",
+    id: 'e4',
+    title: 'New Desk (Wooden)',
     amount: 450,
     date: new Date(2021, 5, 12),
-    location: "Guntur",
   },
 ];
 
 
 const App = () => {
 
-  const [filteredYear, setFilteredYear] = useState('2020');
+    const[expenses,setExpense] = useState(DummyExpense);
+ 
 
-  const filterChangeHandler = selectedYear => {
-    setFilteredYear(selectedYear);
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+    setExpense((prevExpense)=>[expense,...prevExpense]);
   };
 
-
-  
-  const[expenses,setExpense] = useState(DummyExpenses);
-  
-
-  const addExpenseHandler = (expense) =>
-  {
-    console.log('in App.js');
-    console.log(expense);
-
-    //setExpense([expense,...expenses]);
-    
-    setExpense((prevExpense)=>{
-      return([expense,...prevExpense]);
-    });
-
-
-  }
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
-    <Card className='expenses'>
-      <div className="App">
-        <header className="App-header">
-          <NewExpense onAddExpense = {addExpenseHandler}></NewExpense>
-          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-          <>
-            {expenses.map((temp) => {
-              return (
-                <Card className='expenses'>
-                  {" "}
-                  <ExpenceItem
-                    key = {temp.id}
-                    title={temp.title}
-                    amount={temp.amount}
-                    location={temp.location}
-                    date={temp.date}
-                  ></ExpenceItem>
-                </Card>
-              );
-            })}
-          </>
-        </header>
-      </div>
-    </Card>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
   );
-};
+}
 
 export default App;
--- helloworld/src/Components/Expenses/Expenses.js
+++ helloworld/src/Components/Expenses/Expenses.js
@@ -1,52 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./NewExpenses/ExpensesFilter.js";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear => {
+   
+
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterdExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() == filteredYear;
+  });
+
   return (
     <div>
-      <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
-
-                 <ExpenceItem
-                    key = {props.item[0].title}
-                    title={temp.title}
-                    amount={temp.amount}
-                    location={temp.location}
-                    date={temp.date}
-                  ></ExpenceItem>
-
-             
+        <>
+          {filterdExpenses.map((temp) => {
+            return (
+              <ExpenseItem
+                key={temp.id}
+                title={temp.title}
+                amount={temp.amount}
+                location={temp.location}
+                date={temp.date}
+              ></ExpenseItem>
+            );
+          })}
+        </>
       </Card>
     </div>
   );
